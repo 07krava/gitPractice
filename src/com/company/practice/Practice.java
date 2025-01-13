465d90e18efc92122d4f0537dd2d61927f83e0c9
Dmytro_Kravchuk <dmitriy.n.kravchuk@gmail.com>
2025-01-13 20:38:07 +0200

    update our commit

--- src/com/company/practice/Practice.java
+++ src/com/company/practice/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Some Text!!!");
         System.out.println("Second Text!!!");
+        System.out.println("Third Text!!!");
     }
 }
