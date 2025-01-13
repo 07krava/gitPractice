1e2745fda9e3c342b28a254bc5a02053e173a7c4
Dmytro_Kravchuk <dmitriy.n.kravchuk@gmail.com>
2025-01-13 20:35:49 +0200

    First_commit

---
+++ src/com/company/practice/Practice.java
@@ -0,0 +1,8 @@
+package com.company.practice;
+
+public class Practice {
+    public static void main(String[] args) {
+        System.out.println("Some Text!!!");
+        System.out.println("Second Text!!!");
+    }
+}
